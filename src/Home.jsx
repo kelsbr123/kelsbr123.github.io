b149a02f63bdbf01bcc9b722511dd11ea9cdfb1a
Kels Barrett-Rivkin <kelsbr123@gmail.com>
2024-06-05 19:27:13 -0600

    Updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
         <img src="/headshot.jpg" alt="" />
       </div>
       <div className="right paragraph">
+        <h1> A Little About Me</h1>
         <p className="center">
           Hello! I'm Kels! I'm an aspiring software developer from Colorado. I studied computer science with
           a focus on machine learning and artifical intelligence at Colorado State University. I'm looking to break into the software industry
