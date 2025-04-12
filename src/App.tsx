2ab100a324263321b018e73506979ae3aa074dec
kelsbr123 <kelsbr123@gmail.com>
2025-04-11 18:31:14 -0600

    Fixed URL Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import constructionGif from './assets/Under_construction_animated.gif'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a target="_blank">
+          <img src={constructionGif} className="logo" alt="Keystone Ski Resort Logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Sorry! This page is under construction right now.</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
