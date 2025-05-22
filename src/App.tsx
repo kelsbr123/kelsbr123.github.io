9d9199b654b2c7fe6f5c6bff7301b3022692848f
Kels Barrett-Rivkin <kelsbr123@gmail.com>
2025-05-22 16:14:24 -0600

    Added PDFS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import constructionGif from './assets/Under_construction_animated.gif'
 import './App.css'
+import pdf1 from './assets/Lot 4 Keystone Base 1.pdf'
+import pdf2 from './assets/Lot 4 Keystone with Lot 4 Keystone Base 1 Shown.pdf'
 
 function App() {
 
   return (
     <>
       <div className="column">
-        <a target="_blank">
-          <img src={constructionGif} className="logo" alt="Keystone Ski Resort Logo" />
-        </a>
         <h1>OUR GOAL</h1>
         <p>Our goal is to reduce the amount of tax that property owners in keystone pay by dissolving the Keystone Neighbourhood Company, also known as KNC. KNC extracts fees from Keystone owners while
           providing little in return. </p>
       </div>
+      <object data={pdf1+"?#zoom=85"} type="application/pdf" width="1000" height="800"></object>
+      <object data={pdf2+"?#zoom=85"} type="application/pdf" width="1000" height="800"></object>
     </>
   )
 }
